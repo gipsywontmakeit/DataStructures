15a6cc62bee1eee34df03982b7af5aa8f73358d5
gipsywontmakeit <8200689@estg.ipp.pt>
2023-11-16 14:49:51 +0000

    unordered list done

--- src/Lists/UnorderedList.java
+++ src/Lists/UnorderedList.java
@@ -17,11 +17,34 @@ public class UnorderedList<T> extends LinkedList<T> implements UnorderedListADT<
 
     @Override
     public void addToRear(T element) {
+        LinearNode<T> node = new LinearNode<>(element);
 
+        if(isEmpty()) {
+            front = rear = node;
+        } else {
+            rear.setNext(node);
+            rear = node;
+        }
+        count++;
+        modCount++;
     }
 
     @Override
     public void addAfter(T element, T target) {
+        LinearNode<T> node = new LinearNode<>(element);
+        LinearNode<T> current = front;
 
+        while(current != null && !current.getElement().equals(target)) {
+            current = current.getNext();
+        }
+
+        if(current == null) {
+            throw new RuntimeException("Element not found");
+        }
+
+        node.setNext(current.getNext());
+        current.setNext(node);
+        count++;
+        modCount++;
     }
 }
--- src/tester/UnorderedListDemo.java
+++ src/tester/UnorderedListDemo.java
@@ -1,7 +1,17 @@
 package tester;
 
+import Lists.UnorderedList;
+
 public class UnorderedListDemo {
     public static void main(String[] args) {
+        UnorderedList<Integer> unorderedList = new UnorderedList<>();
 
+        unorderedList.addToFront(1);
+        unorderedList.addToFront(2);
+        unorderedList.addToFront(3);
+        unorderedList.addToFront(4);
+        unorderedList.addToRear(5);
+        unorderedList.addAfter(6, 3);
+        System.out.println(unorderedList);
     }
 }
